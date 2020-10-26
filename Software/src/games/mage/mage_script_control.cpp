4188af9d0b38dc3530b2a8519327b83be1d078cc
Tim <tim@twa.ninja>
2020-10-25 19:46:39 -0600

    Added arg typecast into structs for action funcs

--- Software/src/games/mage/mage_script_control.cpp
+++ Software/src/games/mage/mage_script_control.cpp
@@ -87,156 +87,198 @@ void MageScriptControl::runAction(uint32_t actionMemoryAddress)
 	uint8_t romValues[MAGE_NUM_ACTION_ARGS];
 	//Will need to read from ROM for valid data once there's data in the ROM file. -Tim
 	//for now just use the following fake data to test for all arguments:
-	uint8_t actionTypeId = romValues[0];
 	for(int i=0; i<MAGE_NUM_ACTION_ARGS; i++)
 	{ romValues[i] = i*5; }
+	//this will be the last byte read after the 7 args:
+	uint8_t actionTypeId = MageScriptActionTypeId::CHECK_ENTITY_BYTE;
+
+	//get the function for actionTypeId, and feed it the romValues as args:
+	auto func = actionFunctions[actionTypeId];
+	(this->*func)(romValues);
 }
 
 void MageScriptControl::nullAction(uint8_t * args)
 {
+	//nullAction does nothing.
 	return;
 }
 void MageScriptControl::checkEntityByte(uint8_t * args)
 {
+	ActionCheckEntityByte *argStruct = (ActionCheckEntityByte*)args;
 	return;
 }
 void MageScriptControl::checkSaveFlag(uint8_t * args)
 {
+	ActionCheckSaveFlag *argStruct = (ActionCheckSaveFlag*)args;
 	return;
 }
 void MageScriptControl::checkIfEntityIsInGeometry(uint8_t * args)
 {
+	ActionCheckifEntityIsInGeometry *argStruct = (ActionCheckifEntityIsInGeometry*)args;
 	return;
 }
 void MageScriptControl::checkForButtonPress(uint8_t * args)
 {
+	ActionCheckForButtonPress *argStruct = (ActionCheckForButtonPress*)args;
 	return;
 }
 void MageScriptControl::checkForButtonState(uint8_t * args)
 {
+	ActionCheckForButtonState *argStruct = (ActionCheckForButtonState*)args;
 	return;
 }
 void MageScriptControl::checkDialogResponse(uint8_t * args)
 {
+	ActionCheckDialogResponse *argStruct = (ActionCheckDialogResponse*)args;
 	return;
 }
 void MageScriptControl::compareEntityName(uint8_t * args)
 {
+	ActionCompareEntityName *argStruct = (ActionCompareEntityName*)args;
 	return;
 }
 void MageScriptControl::delay(uint8_t * args)
 {
+	ActionDelay *argStruct = (ActionDelay*)args;
 	return;
 }
 void MageScriptControl::nonBlockingDelay(uint8_t * args)
 {
+	ActionNonBlockingDelay *argStruct = (ActionNonBlockingDelay*)args;
 	return;
 }
 void MageScriptControl::setPauseState(uint8_t * args)
 {
+	ActionSetPauseState *argStruct = (ActionSetPauseState*)args;
 	return;
 }
 void MageScriptControl::setEntityByte(uint8_t * args)
 {
+	ActionSetEntityByte *argStruct = (ActionSetEntityByte*)args;
 	return;
 }
 void MageScriptControl::setSaveFlag(uint8_t * args)
 {
+	ActionSetSaveFlag *argStruct = (ActionSetSaveFlag*)args;
 	return;
 }
 void MageScriptControl::setPlayerControl(uint8_t * args)
 {
+	ActionSetPlayerControl *argStruct = (ActionSetPlayerControl*)args;
 	return;
 }
 void MageScriptControl::setEntityInteractScript(uint8_t * args)
 {
+	ActionSetEntityInteractScript *argStruct = (ActionSetEntityInteractScript*)args;
 	return;
 }
 void MageScriptControl::setEntityTickScript(uint8_t * args)
 {
+	ActionSetEntityTickScript *argStruct = (ActionSetEntityTickScript*)args;
 	return;
 }
 void MageScriptControl::setMapTickScript(uint8_t * args)
 {
+	ActionSetMapTickScript *argStruct = (ActionSetMapTickScript*)args;
 	return;
 }
 void MageScriptControl::setEntityType(uint8_t * args)
 {
+	ActionSetEntityType *argStruct = (ActionSetEntityType*)args;
 	return;
 }
 void MageScriptControl::setHexCursorLocation(uint8_t * args)
 {
+	ActionSetHexCursorLocation *argStruct = (ActionSetHexCursorLocation*)args;
 	return;
 }
 void MageScriptControl::setHexBit(uint8_t * args)
 {
+	ActionSetHexBit *argStruct = (ActionSetHexBit*)args;
 	return;
 }
 void MageScriptControl::unlockHaxCell(uint8_t * args)
 {
+	ActionUnlockHaxCell *argStruct = (ActionUnlockHaxCell*)args;
 	return;
 }
 void MageScriptControl::lockHaxCell(uint8_t * args)
 {
+	ActionLockHaxCell *argStruct = (ActionLockHaxCell*)args;
 	return;
 }
 void MageScriptControl::loadMap(uint8_t * args)
 {
+	ActionLoadMap *argStruct = (ActionLoadMap*)args;
 	return;
 }
 void MageScriptControl::screenShake(uint8_t * args)
 {
+	ActionScreenShake *argStruct = (ActionScreenShake*)args;
 	return;
 }
 void MageScriptControl::screenFadeOut(uint8_t * args)
 {
+	ActionScreenFadeOut *argStruct = (ActionScreenFadeOut*)args;
 	return;
 }
 void MageScriptControl::screenFadeIn(uint8_t * args)
 {
+	ActionScreenFadeIn *argStruct = (ActionScreenFadeIn*)args;
 	return;
 }
 void MageScriptControl::showDialog(uint8_t * args)
 {
+	ActionShowDialog *argStruct = (ActionShowDialog*)args;
 	return;
 }
 void MageScriptControl::setRenderableFont(uint8_t * args)
 {
+	ActionSetRenderableFont *argStruct = (ActionSetRenderableFont*)args;
 	return;
 }
 void MageScriptControl::moveEntityToGeometry(uint8_t * args)
 {
+	ActionMoveEntityToGeometry *argStruct = (ActionMoveEntityToGeometry*)args;
 	return;
 }
 void MageScriptControl::moveEntityAlongGeometry(uint8_t * args)
 {
+	ActionMoveEntityAlongGeometry *argStruct = (ActionMoveEntityAlongGeometry*)args;
 	return;
 }
 void MageScriptControl::loopEntityAlongGeometry(uint8_t * args)
 {
+	ActionLoopEntityAlongGeometry *argStruct = (ActionLoopEntityAlongGeometry*)args;
 	return;
 }
 void MageScriptControl::moveCameratoGeometry(uint8_t * args)
 {
+	ActionMoveCameraToGeometry *argStruct = (ActionMoveCameraToGeometry*)args;
 	return;
 }
 void MageScriptControl::moveCameraAlongGeometry(uint8_t * args)
 {
+	ActionMoveCameraAlongGeometry *argStruct = (ActionMoveCameraAlongGeometry*)args;
 	return;
 }
 void MageScriptControl::loopCameraAlongGeometry(uint8_t * args)
 {
+	ActionLoopCameraAlongGeometry *argStruct = (ActionLoopCameraAlongGeometry*)args;
 	return;
 }
 void MageScriptControl::setEntityDirection(uint8_t * args)
 {
+	ActionSetEntityDirection *argStruct = (ActionSetEntityDirection*)args;
 	return;
 }
 void MageScriptControl::setHexEditorState(uint8_t * args)
 {
+	ActionSetHexEditorState *argStruct = (ActionSetHexEditorState*)args;
 	return;
 }
 void MageScriptControl::setHexEditorDialogMode(uint8_t * args)
 {
+	ActionSetHexEditorDialogMode *argStruct = (ActionSetHexEditorDialogMode*)args;
 	return;
 }
