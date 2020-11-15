b8797416b9e097a29d22b8b8d953d708fd4d4ef7
Tim <tim@twa.ninja>
2020-11-15 13:55:50 -0700

    Better button test code with software debounce

--- Software/src/modules/keyboard.c
+++ Software/src/modules/keyboard.c
@@ -3,6 +3,7 @@
 
 volatile uint32_t keyboard_mask = 0;
 keyboard_evt_handler_t keyboard_evt_handler = NULL;
+uint32_t last_keyboard_interrupt_time = 0;
 
 #ifdef DC801_EMBEDDED
 #include "nrfx_gpiote.h"
@@ -40,10 +41,16 @@ bool is_keyboard_interrupt(void){
 
 uint32_t get_keyboard_mask(void)
 {
-	if(is_keyboard_interrupt())
+	uint32_t now = millis();
+	//need to wait at least KEYBOARD_DEBOUNCE_TIME ms before we try to update again:
+	if(
+		is_keyboard_interrupt() &&
+		(now - last_keyboard_interrupt_time) > KEYBOARD_DEBOUNCE_TIME
+	)
 	{
 		uint8_t *ptr = (uint8_t *)&keyboard_mask;
 		i2cMasterRead(KEYBOARD_ADDRESS, ptr, sizeof(uint32_t));
+		last_keyboard_interrupt_time = now;
 	}
 	return keyboard_mask;
 }
