493dd13061062731199240f5289814c4dfa7d441
Tim <tim@twa.ninja>
2020-11-22 20:59:10 -0700

    Testing RAM Corruption Bug w/ Screen Printing

--- Software/src/engine/FrameBuffer.cpp
+++ Software/src/engine/FrameBuffer.cpp
@@ -1206,12 +1206,6 @@ void draw_raw_async(int16_t x, int16_t y, uint16_t w, uint16_t h, uint16_t *p_ra
 		
 		uint32_t count = MIN(320*80*2, bytecount);
 
-		//Hang out until LCD is free
-		while (ili9341_is_busy()) 
-		{
-			APP_ERROR_CHECK(sd_app_evt_wait());
-		}
-
 		ili9341_push_colors_fast((uint8_t*)p_raw, count);
 
 		p_raw += count / 2; //convert to uint16_t count
