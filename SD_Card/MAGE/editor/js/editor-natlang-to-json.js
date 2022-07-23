97172478ea75dd821083062a5597000a3314d6d9
Alamedyang <alamedyang@gmail.com>
2022-07-23 00:36:37 -0600

    Natlang test run; XA room hax/door try messages
    - small natlang QOL improvements
    - small natlang inconsistencies fixed
    - some general natlang refactoring and comment improvements
    - new folder for .mgs (natlang) files

--- SD_Card/MAGE/editor/js/editor-natlang-to-json.js
+++ SD_Card/MAGE/editor/js/editor-natlang-to-json.js
@@ -1,5 +1,6 @@
-// ws and eatWS return a position directly on success, all others return
-// an object containing (at least) a newPosition property
+// ws and eatWS return a position directly on success; all others return
+// an object containing (at least) a newPosition property (or false
+// upon failure)
 
 patternParse = {
 	ws: function (inputString, pos) {
@@ -41,43 +42,41 @@ patternParse = {
 		}
 		// skipping over white space
 		pos = patternParse.eatWS(inputString, pos);
-		// finding some small chunk of a "modest" string
-		commentLabelObject = patternParse.miniString(inputString, pos);
-		if (!commentLabelObject) {
-			return false;
-		}
-		pos = commentLabelObject.newPosition;
-		// skipping over white space
-		pos = patternParse.eatWS(inputString, pos);
-		// matching :
-		if (inputString.substring(pos).match(/^:/)) {
-			pos += 1;
-		} else {
-			return false;
-		}
-		// skipping over white space
-		pos = patternParse.eatWS(inputString, pos);
 		// finding literally everything except the next */
 		var matches = inputString.substring(pos).match(/^(.*?)\*\//);
+		// TODO: PROBLEM: a pair of double quotes breaks this match!!
 		if (matches[0].length > 0) {
 			pos += matches[0].length;
 		} else {
 			return false;
 		}
-		// WRAP UP
-		let commentBody = matches[1];
-		var commentLabel = commentLabelObject.value;
-		return {
-			startPosition: startPos,
-			newPosition: pos,
-			// used only for debugging
-			matchedText: inputString.substring(startPos, pos),
-			charCoords: findLineAndCharNumbers(inputString, startPos),
-			type: 'comment',
-			value: {
-				commentLabel: commentLabel,
-				commentBody: commentBody,
-			},
+		// COMMENT LABEL DETECTION
+		var commentString = matches[1].trim();
+		// finding some small chunk of a "modest" string
+		commentLabelObject = patternParse.miniString(commentString, 0);
+		if (commentLabelObject) {
+			var labelPos = commentLabelObject.newPosition;
+			var commentResult = {
+				startPosition: startPos,
+				newPosition: pos,
+				// used only for debugging
+				matchedText: inputString.substring(startPos, pos),
+				charCoords: findLineAndCharNumbers(inputString, startPos),
+			};
+			if (commentString.substring(labelPos).match(/^:/)) {
+				cutPos = labelPos + 1;
+				commentResult.type = 'comment';
+				commentResult.value = {
+					commentLabel: commentString.substring(0,labelPos),
+					commentBody: commentString.substring(cutPos).trim(),
+				};
+			} else {
+				commentResult.type = 'comment-unlabeled';
+				commentResult.value = commentString;
+			}
+			return commentResult;
+		} else {
+			return false;
 		}
 	},
 	scriptName: function (inputString, pos) {
@@ -169,6 +168,8 @@ patternParse = {
 };
 
 var findLineAndCharNumbers = function (inputString, pos) { // `12345\n789AB`, 8
+	// Turns a string and a char position into
+	// a more reasonable line and col (plus bonus info)
 	var substring = inputString.substring(0,pos); // `12345\n789`
 	var splits = substring.split('\n') // [ '12345', '789' ]
 	var lineNumber = splits.length; // 2
@@ -182,121 +183,42 @@ var findLineAndCharNumbers = function (inputString, pos) { // `12345\n789AB`, 8
 	};
 };
 
-var parseNatlangBlock = function (inputString) {
-	var oldPos = null;
-	var pos = pos = patternParse.eatWS(inputString, pos);
-	var result = [];
-	while (
-		oldPos !== pos
-		&& pos < inputString.length
-	) {
-		var match = patternParse.text(inputString, pos)
-		if (match) {
-			result.push(match);
-			oldPos = pos;
-			pos = pos = patternParse.eatWS(inputString, match.newPosition);
-		} else {
-			oldPos = pos;
-			var errorCoords = findLineAndCharNumbers(inputString, pos);
-			var arrow = '~'.repeat(errorCoords.col) + '^';
-			console.error(`╓ Pattern parser failure! Line ${errorCoords.row}:${errorCoords.col}`);
-			console.error('║ ' + `${errorCoords.lineString}`);
-			console.error('╙~' + arrow);
-			return false;
-		}
-	};
-	// console.log(`Natlang block (length: ${inputString.length}) parsed to ${pos}`);
-	return result;
-};
-
-var parseNatlangStream = function (stream) {
-	var scripts = {};
-	var scriptName = '';
-	var scriptBody = [];
-	var workingTokens = [];
-	var treeRef = natlangParseTree;
-	for (var index = 0; index < stream.length; index++) {
-		var token = stream[index];
-		if (token.type === 'scriptName') {
-			if (scriptName.length > 0) {
-				scripts[scriptName] = scriptBody;
-			}
-			scriptName = token.value;
-			scriptBody = [];
-		} else if (token.type === 'comment') {
-			var lastScript = scriptBody[scriptBody.length - 1];
-			// TODO: break gracefully if a comment is above the first action
-			lastScript[token.value.commentLabel] = token.value.commentBody.trim();
-		} else { // ['block','1ms','close','hex','editor']
-			var currentBranches = Object.keys(treeRef); // [ "block", "make", "$bool", "erase", ...]
-			var word = token.value; // 'block'
-			var tokenPotential = natlangVariableReport(word)
-				.map(function (item) {
-					return '$' + item;
-				})
-				.concat([word]);
-			var potentialBranches = tokenPotential
-				.filter(function (item) {
-					return currentBranches.includes(item);
-				})
-			if (potentialBranches.length === 0) {
-				var coords = token.charCoords || null;
-				var message = coords
-					? `Natlang parse error at line ${coords.row}:${coords.col}`
-					: `Natlang parse error at token index ${index}`;
-				console.error(message);
-				console.error(`    Token: ${token.value}`);
-				console.error(`    Expected: ${currentBranches.join(', ')}`);
-				scripts = null;
-				break
-			} else if (potentialBranches.length === 1) {
-				var branch = potentialBranches[0];
-				workingTokens.push(word);
-				treeRef = treeRef[branch];
-			}
-		}
-		if (Object.keys(treeRef).length === 1 && Object.keys(treeRef).includes("END")) {
-			scriptBody.push(translateTokensToJSON.action(workingTokens));
-			workingTokens = [];
-			treeRef = natlangParseTree;
-		}
-	};
-	scripts[scriptName] = scriptBody;
-	return scripts;
-}
-
 var getDictionaryItemFromTokens = function (tokens) {
-	var sameLengthPatterns = natlangDictionary.filter(function (pattern) {
-		var patternSplits = pattern.pattern.split(' ');
-		if (patternSplits.length !== tokens.length) {
-			return false;
-		} else {
-			return true;
-		}
-	})
-	var matches = sameLengthPatterns.filter(function (pattern) {
-		var patternSplits = pattern.pattern.split(' ');
-		var result = true;
-		for (var index = 0; index < patternSplits.length; index++) {
-			var patternWord = patternSplits[index];
-			var testWord = tokens[index];
-			var keyWord = patternWord[0] !== '$'
-			if (keyWord) {
-				if (patternWord !== testWord) {
-					result = false;
-					break
-				}
+	// Turns a set of known-to-be-good tokens into a single dictionary item
+	// (Returns false upon failure)
+	var sameLengthPatterns = natlangDictionary
+		.filter(function (pattern) {
+			var patternSplits = pattern.pattern.split(' ');
+			if (patternSplits.length !== tokens.length) {
+				return false;
 			} else {
-				var varType = patternWord.substring(1);
-				var validVar = natlangVariableValidate[varType](testWord);
-				if (!validVar) {
-					result = false;
-					break
+				return true;
+			}
+		})
+	var matches = sameLengthPatterns
+		.filter(function (pattern) {
+			var patternSplits = pattern.pattern.split(' ');
+			var result = true;
+			for (var index = 0; index < patternSplits.length; index++) {
+				var patternWord = patternSplits[index];
+				var testWord = tokens[index];
+				var keyWord = patternWord[0] !== '$'
+				if (keyWord) {
+					if (patternWord !== testWord) {
+						result = false;
+						break
+					}
+				} else {
+					var varType = patternWord.substring(1);
+					var validVar = natlangVariableValidate[varType](testWord);
+					if (!validVar) {
+						result = false;
+						break
+					}
 				}
 			}
-		}
-		return result;
-	})
+			return result;
+		})
 	if (matches.length === 1) {
 		return matches[0];
 	} else if (matches.length > 1) {
@@ -304,16 +226,19 @@ var getDictionaryItemFromTokens = function (tokens) {
 		// console.warn(matches)
 		return matches.length;
 	} else {
-		return null;
+		return false;
 	}
 };
 
 var getActionNameFromTokens = function (tokens) {
+	// Turns a set of known-to-be-good tokens into an action name
 	var dictionaryItem = getDictionaryItemFromTokens(tokens);
 	return dictionaryItem.action;
 };
 
 var translateTokensToJSON = {
+	// Use translateTokensToJSON.action() to turn a set of
+	// known-to-be-good tokens into its equivalent JSON
 	action: function (tokens) {
 		var dictionary = getDictionaryItemFromTokens(tokens);
 		if (!dictionary) {
@@ -352,6 +277,7 @@ var translateTokensToJSON = {
 		})
 		return result;
 	},
+	// Everything below is used by the first function
 	comment: function (string) {
 		splits = string
 			.replace('// ','')
@@ -427,6 +353,8 @@ var boolMap = {
 };
 
 var natlangVariableValidate = {
+	// Determines whether an input string qualifies as a specific
+	// natlang variable; returns true or false
 	bool: function (string) {
 		return Object.keys(boolMap).includes(string);
 	},
@@ -485,6 +413,8 @@ var natlangVariableValidate = {
 };
 
 var natlangVariableReport = function (variable) {
+	// Takes a string and returns an array of all its
+	// valid natlang variable types
 	var result = [];
 	var types = Object.keys(natlangVariableValidate);
 	types.forEach(function (type) {
@@ -493,4 +423,103 @@ var natlangVariableReport = function (variable) {
 		}
 	})
 	return result;
+};
+
+var parseNatlangBlock = function (inputString) {
+	// Turns natlang plaintext into a stream of categorized tokens
+	// (Returns false upon failure)
+	var oldPos = null;
+	var pos = pos = patternParse.eatWS(inputString, pos);
+	var tokenStream = [];
+	while (
+		oldPos !== pos
+		&& pos < inputString.length
+	) {
+		var match = patternParse.text(inputString, pos)
+		if (match) {
+			tokenStream.push(match);
+			oldPos = pos;
+			pos = pos = patternParse.eatWS(inputString, match.newPosition);
+		} else {
+			oldPos = pos;
+			var errorCoords = findLineAndCharNumbers(inputString, pos);
+			var arrow = '~'.repeat(errorCoords.col) + '^';
+			console.error(`╓ Pattern parser failure! Line ${errorCoords.row}:${errorCoords.col}`);
+			console.error('║ ' + `${errorCoords.lineString}`);
+			console.error('╙~' + arrow);
+			return false;
+		}
+	};
+	return tokenStream;
+};
+var parseNatlangStream = function (tokenStream) {
+	// Turns a natlang token stream into the actual JSON
+	var json = {};
+	var scriptName = '';
+	var scriptBody = [];
+	var workingTokens = [];
+	var treeRef = natlangParseTree;
+	for (var index = 0; index < tokenStream.length; index++) {
+		var token = tokenStream[index];
+		if (token.type === 'scriptName') {
+			if (scriptName.length > 0) {
+				json[scriptName] = scriptBody;
+			}
+			scriptName = token.value;
+			scriptBody = [];
+		} else if (token.type === 'comment') {
+			var lastScript = scriptBody[scriptBody.length - 1];
+			// TODO: break gracefully if a comment is above the first action
+			lastScript[token.value.commentLabel] = token.value.commentBody.trim();
+		} else if (token.type === 'comment-unlabeled') {
+			// NOTHING LOL (or TODO make them generically and sequentially named)
+		} else { // ['block','1ms','close','hex','editor']
+			var currentBranches = Object.keys(treeRef); // [ "block", "make", "$bool", "erase", ...]
+			var word = token.value; // 'block'
+			var tokenPotential = natlangVariableReport(word)
+				.map(function (item) {
+					return '$' + item;
+				})
+				.concat([word]);
+			var potentialBranches = tokenPotential
+				.filter(function (item) {
+					return currentBranches.includes(item);
+				})
+			if (potentialBranches.length === 0) {
+				var coords = token.charCoords || null;
+				var message = coords
+					? `Natlang parse error at line ${coords.row}:${coords.col}`
+					: `Natlang parse error at token index ${index}`;
+				console.error(message);
+				console.error(`    Token: ${token.value}`);
+				console.error(`    Expected: ${currentBranches.join(', ')}`);
+				json = null;
+				break
+			} else if (potentialBranches.length === 1) {
+				var branch = potentialBranches[0];
+				workingTokens.push(word);
+				treeRef = treeRef[branch];
+			}
+		}
+		if (Object.keys(treeRef).length === 1 && Object.keys(treeRef).includes("END")) {
+			scriptBody.push(translateTokensToJSON.action(workingTokens));
+			workingTokens = [];
+			treeRef = natlangParseTree;
+		}
+	};
+	json[scriptName] = scriptBody;
+	return json;
+}
+
+// THE BELOW IS ALL THAT MATTERS FOR GENERAL PURPOSES:
+
+var translateNatlangToJSON = function (natlangBlock) {
+	// Insert natlang plaintext, receive JSON
+	// (Returns false upon failure)
+	var stream = parseNatlangBlock(natlangBlock);
+	if (stream === false) {
+		console.error('Translation aborted!');
+		return false;
+	}
+	return parseNatlangStream(stream);
 };
