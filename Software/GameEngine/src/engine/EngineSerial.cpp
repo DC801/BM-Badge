cf072f7fc6d82f5d8084e2a67a58cc11740b89e4
Tawnos <tawnos+github@gmail.com>
2023-03-02 16:34:24 -0800

    Fix engine input blocking

--- Software/GameEngine/src/engine/EngineSerial.cpp
+++ Software/GameEngine/src/engine/EngineSerial.cpp
@@ -13,39 +13,52 @@ void (*on_command_function_pointer)(char *commandString) = nullptr;
 
 #ifndef DC801_EMBEDDED
 #define FILE_DESCRIPTOR_STDIN 0
-void EngineInputDesktopGetCommandStringFromStandardIn ()
+void EngineInputDesktopGetCommandStringFromStandardIn()
 {
-	memset(
-		command_buffer,
-		0,
-		COMMAND_BUFFER_SIZE
-	);
+    memset(
+        command_buffer,
+        0,
+        COMMAND_BUFFER_SIZE
+    );
 #ifndef WIN32
 
-	std::ios_base::sync_with_stdio(false);
-	std::string str = "";
-	std::getline(std::cin, str, '\n');
+    std::ios_base::sync_with_stdio(false);
+    auto inChar = char{ 0 };
+    auto charsRead = int{ 0 };
+    auto lineRead = bool{ false };
 
-	if (str.length() > 0) {
-		command_buffer_length = str.length();
-		command_buffer[command_buffer_length] = 0x00;
-		was_command_entered = str.length() > 0;
-		/*
-		printf(
-			"stdin"
-			"bytes_read: %d"
-			"| value: %d"
-			"| char: %c"
-			"| command_buffer_length: %d"
-			"| string: %s\n",
-			bytes_read,
-			command_buffer[0],
-			command_buffer[0],
-			command_buffer_length,
-			command_buffer
-		);
-		*/
-	}
+    do
+    {
+        charsRead = std::cin.readsome(&inChar, 1);
+        if (charsRead == 1)
+        {
+            if (inChar == '\n')
+            {
+                lineRead = true;
+            }
+            else
+            {
+                command_buffer[command_buffer_length] = inChar;
+                command_buffer_length++;
+                /*
+                printf(
+                    "stdin"
+                    "bytes_read: %d"
+                    "| value: %d"
+                    "| char: %c"
+                    "| command_buffer_length: %d"
+                    "| string: %s\n",
+                    bytes_read,
+                    command_buffer[0],
+                    command_buffer[0],
+                    command_buffer_length,
+                    command_buffer
+                );
+                */
+            }
+        }
+    } while (charsRead != 0 && !lineRead);
+    was_command_entered = command_buffer_length > 0;
 #endif
 }
 
@@ -54,53 +67,53 @@ void EngineInputDesktopGetCommandStringFromStandardIn ()
 void EngineSendSerialMessage (const char *message)
 {
 #ifdef DC801_EMBEDDED
-	std::string message_with_crlf = std::regex_replace(
-		message,
-		std::regex("\n"),
-		"\r\n"
-	);
-	send_serial_message(message_with_crlf.c_str());
+    std::string message_with_crlf = std::regex_replace(
+        message,
+        std::regex("\n"),
+        "\r\n"
+    );
+    send_serial_message(message_with_crlf.c_str());
 #else
-	printf("%s", message);
-	fflush(stdout);
+    printf("%s", message);
+    fflush(stdout);
 #endif
 }
 
 void EngineSerialRegisterEventHandlers (
-	void (*on_start)(),
-	void (*on_command)(char *commandString)
+    void (*on_start)(),
+    void (*on_command)(char *commandString)
 ) {
-	on_start_function_pointer = on_start;
-	on_command_function_pointer = on_command;
+    on_start_function_pointer = on_start;
+    on_command_function_pointer = on_command;
 }
 
 void EngineHandleSerialInput ()
 {
 #ifdef DC801_EMBEDDED
-	handle_usb_serial_input();
+    handle_usb_serial_input();
 #else
-	EngineInputDesktopGetCommandStringFromStandardIn();
+    EngineInputDesktopGetCommandStringFromStandardIn();
 #endif
 
-	if(was_serial_started) {
-		if(on_start_function_pointer != nullptr) {
-			on_start_function_pointer();
-		}
-		was_serial_started = false;
-	}
-	if(was_command_entered) {
-		if(on_command_function_pointer != nullptr) {
-			on_command_function_pointer(command_buffer);
-		}
-		EngineSendSerialMessage("> ");
-		memset(
-			command_buffer,
-			0,
-			command_buffer_length
-		);
-		command_buffer_length = 0;
-		was_command_entered = false;
-	}
+    if(was_serial_started) {
+        if(on_start_function_pointer != nullptr) {
+            on_start_function_pointer();
+        }
+        was_serial_started = false;
+    }
+    if(was_command_entered) {
+        if(on_command_function_pointer != nullptr) {
+            on_command_function_pointer(command_buffer);
+        }
+        EngineSendSerialMessage("> ");
+        memset(
+            command_buffer,
+            0,
+            command_buffer_length
+        );
+        command_buffer_length = 0;
+        was_command_entered = false;
+    }
 }
 
 
