e42334557fada72817d05e45a3c263241339b0c0
Alamedyang <alamedyang@gmail.com>
2023-06-18 00:38:36 -0600

    File names and script names refactoring

---
+++ SD_Card/MAGE/scenario_source_files/mgs/ch2/ch2-map-PLANNING.md
@@ -0,0 +1,202 @@
+# USB dungeon serial "map" (ideas)
+
+```
+Nethack-like
+-------------------------------
+|  /----\      -       M A P  |
+|  |....|     /.\             |
+|  ------   /.....\           |
+|           -------           |
+|            |...|   -------  |
+|   -----    |...|   |.....|  |
+|  /.....\   |...|   -------  |
+|  \...../   |...|    |...|   |
+|   -----    -------------|   |
+|   |...|    |...|........|   |
+|   -----------------------   |
+|   |........|...|            |
+|   |---------------          |
+|          |...@...|          |
+|          ---------          |
+-------------------------------
+
+ /----\      -
+ | 99 |     / \
+ ------   / 14  \
+          -------  -------
+  -----    |   |   | 33|*|   *=34
+ /     \   |13 |   -------
+ \ 23  /   |   |    |32 |
+  -----    --------------
+  |22 |    |12 |    31  |
+  |----------------------
+  |   21   |11 |
+  |---------------
+         |   1   |
+         ---------
+
+// ENTRANCE
+1. Exa room
+	- XA: intercom with XB (outside) and XC in room 99
+	- Save floppy
+	- ITEM: computer enclosure + motherboard
+
+// MAIN BRANCH
+11. Front hallway
+	- ITEM: Monitor
+12. Back hallway
+	- ITEM: phonograph
+13. Throne room
+	- the royal advisor (a goldfish, ruling in King Gibson's absence)
+	- ITEM: goldfish (CPU item 1)
+14. King Gibson's bedroom
+	- ITEM: grandfather clock
+
+// INDUSTRIAL AREA
+21. Blacksmith / workshop area
+	- ITEM: abacus (CPU item 2)
+	- (will combine goldfish with abacus)
+22. Server room
+	- ITEM: power supply
+23. Geothermal power plant
+	- Deliver Cactus Cooler to them to receive ITEM
+	- ITEM: Heat sink
+
+// FOOD STUFFS
+31. Grand hall
+	- Copy Pasta bar
+	- Vending machine
+	- Rock band
+		- ITEM: keyboard (or possibly keytar)
+32. Kitchen
+	- ITEM: RAM chips (from vending machine)
+33. Hydroponics area
+	- ITEM: mouse (an actual rodent)
+34. Pantry
+	- ITEM: SSD/HD (plate)
+
+// THE END
+99. Secret room (teleport only)
+	- Lambda
+	- XC: intercom with XB (outside) and XA in room 1
+	- ITEM: mainframe OS
+
+// TOWN
+- ITEM: cactus cooler (needed for heat sink)
+```
+
+## ZONES
+
+```
+ 0-------------------------------
+ 1|  /----\      -       M A P  |    99        14
+ 2|  |.@..|     /.\             |    99        14
+ 3|  ------   /..@..\           |    99        14
+ 4|           -------           |          13  14
+ 5|            |...|   -------  |          13              33  34
+ 6|   -----    |.@.|   |.@.|@|  |      23  13              33  34
+ 7|  /..@..\   |...|   -------  |      23  13          32  33  34
+ 8|  \...../   |...|    |.@.|   |      23  13          32
+ 9|   -----    --------------   |  22  23  13  12  31  32
+10|   |.@.|    |.@.|...@....|   |  22          12  31
+11|   -----------------------   |  22  21  11  12  31
+12|   |...@....|.@.|            |      21  11
+13|   ----------------          |      21  11
+14|          |...@...|          |          1
+15|          ---------          |
+16-------------------------------
+```
+
+## STORY PROGRESSION
+
+(Only numbered items count as story flags)
+
+Choose which "round" with the `ch2-storyflag-round` variable. Checkboxes refer to script scaffolding (the exact convos may be still unwritten); see `ch2-PLANNING.md` for the sprites checklist.
+
+- INTRO
+	- ch2-storyflag-round = 0
+		- [ ] Village elders are all right outside
+		- [ ] Villagers direct you to western dungeon (castle)
+	- [x] Intro to Lambda
+		- [ ] Connecting the artifact on Windows
+		- [ ] Connecting the artifact on MacOS / Linux
+		- [x] Connecting the artifact on web
+		- [x] Intro to artifact
+	- [x] Installation Wizard
+		- [x] Introduction
+		- [x] Waiting for completion
+	- Starting item: enclosure/motherboard (room 1)
+
+- FIRST ROUND: normal
+	- ch2-storyflag-round = 1
+		- [ ] Village elders are in the library or various places (not in the same room though)
+		- [ ] Villagers ask how things are going
+	- [x] (1.) `monitor` (room 11)
+	- [x] (2.) `heatsink` (room 23)
+		- [x] `cactuscooler` (Bob's club)
+	- [x] (3.) `powersupply` (room 22)
+	- [x] Bert secret cutscene happens when all of the above are done
+	- [x] Lambda gives next round of the parts list
+
+- SECOND ROUND: abstract
+	- ch2-storyflag-round = 2
+		- [ ] Village elders are all around village
+	- [x] (4.) `keyboard` (keytar) (room 31)
+	- [ ] (5.) `mouse` (rodent) (room 33)
+	- [ ] (6.) `harddrive`
+		- [x] `plate` (dinner plate) (room 34)
+		- [ ] `needle` (phonograph needle) (room 12)
+	- [x] (7.) `ramchips` (bag of Doritos) (room 32)
+	- [ ] Bert "any messages yet?" convo happens when all of the above are done
+	- [ ] Lambda gives next round of the parts list
+
+- THIRD ROUND: desperate
+	- ch2-storyflag-round = 3
+		- [ ] Village elders are both in the inner sanctum
+	- [ ] (8.) `clock` (grandfather clock) (room 14)
+	- [ ] (9.) `cpu`
+		- [ ] `goldfish` (same goldfish sprite as bedroom) (room 13)
+		- [x] `abacus` (room 21)
+	- [ ] Lambda shows you how to `warp` and invites you to room #99 
+
+- FINAL ROUND: software
+	- ch2-storyflag-round = 4
+		- [ ] Village elders are waiting outside
+	- [ ] (10.) `mainframeos` (room 99) (might be handled differently)
+		- [ ] Cutscene: OS installation wizard
+	- [ ] End cutscene
+	- [ ] Credits
+
+- AFTERWARD
+	- ch2-storyflag-round = 5
+		- [ ] Villagers speculate on the next area: software!
+		- [ ] Village elders say how you can have a break now
+
+### Scripts needed
+
+For each item: (Copy existing pattern wherever these are found)
+
+1. Put entity on a map (`ENTITY`) and set `on_interact` = `ch2-touch-ITEM`
+2. In file `ch2-admin.mgs`:
+	1. Script `ch2-report-inventory` — Add serial dialog `\tITEM` display check (using flag `ch2-carrying-ITEM`)
+	2. Script `ch2-count-flags` — Add tally logic check (using flag `ch2-installed-ITEM`)
+	3. Script `ch2-interact-mainframe` — Add installation dialog (using flags `ch2-installed-ITEM` and `ch2-carrying-ITEM`)
+3. In script file for the map room:
+	1. Room's `on_load` — Add entity hide behavior (using flags `ch2-installed-ITEM` and `ch2-carrying-ITEM`)
+	2. Make script `ch2-hide-ITEM`
+		- `teleport entity ENTITY to geometry hiding-spot`
+		- `set entity ENTITY name to " "`
+	3. Make script `ch2-touch-ITEM`
+		- (Copy existing pattern)
+	4. Make script `ch2-install-ITEM`
+		- `set flag ch2-carrying-ITEM to false`
+		- `set flag ch2-installed-ITEM to true`
+	5. Make script `look-ITEM`
+4. In file `ch2-serial-toot.mgs`
+	1. Make serial dialog `ch2-describe-ITEM`
+	2. Add a new substep to script `ch2-toot-step-5X`
+5. In file `ch2-man-tree.mgs`
+	1. Make script `ch2-describe-ITEM`: `show serial dialog ch2-describe-ITEM`
+	2. Make script `ch2-lambda-parts-ITEM` (copy an existing)
+	3. Add to `ch2-lambda-topic-parts` (using flag `ch2-installed-ITEM`)
+	4. Add to script `ch2-lambda-topic-parts-q` (if multiple words, add a no-spaces option, too)
---
+++ SD_Card/MAGE/scenario_source_files/mgs/ch2/ch2-serial-map.js
@@ -0,0 +1,346 @@
+var lispish =
+[
+	["-------------------------------"],
+	[//row 1
+		"|  ",
+		["=99", "/----\\"],
+		"      ",
+		["=14", "-"],
+		"       M A P  |"
+	],
+	[//row 2
+		"|  ",
+		["=99", "|.@..|"],
+		"     ",
+		["=14", "/.\\"],
+		"             |"
+	],
+	[//row 3
+		"|  ",
+		["=99", "------"],
+		"   ",
+		["=14", "/..@..\\"],
+		"           |"
+	],
+	[//row 4
+		"|           ",
+		["=14", "-"],
+		["=14||13", "-----"],
+		["=14", "-"],
+		"           |"
+	],
+	[//row 5
+		"|            ",
+		["=13", "|...|"],
+		"   ",
+		["=33", "----"],
+		["=33||34", "-"],
+		["=34", "--"],
+		"  |"
+	],
+	[//row 6
+		"|   ",
+		["=23", "-----"],
+		"    ",
+		["=13", "|.@.|"],
+		"   ",
+		["=33", "|.@."],
+		["=33||34", "|"],
+		["=34", "@|"],
+		"  |"
+	],
+	[//row 7
+		"|  ",
+		["=23", "/..@..\\"],
+		"   ",
+		["=13", "|...|"],
+		"   ",
+		["=33", "-"],
+		["=33||32", "---"],
+		["=33||32||34", "-"],
+		["=32||34", "-"],
+		["=34", "-"],
+		"  |"
+	],
+	[//row 8
+		"|  ",
+		["=23", "\\...../"],
+		"   ",
+		["=13", "|...|"],
+		"    ",
+		["=32", "|.@.|"],
+		"   |"
+	],
+	[//row 9
+		"|   ",
+		["=22||23", "-----"],
+		"    ",
+		["=12||13", "----"],
+		["=12||13||31", "-"],
+		["=31", "----"],
+		["=31||32", "-----"],
+		"   |"
+	],
+	[//row 10
+		"|   ",
+		["=22", "|.@.|"],
+		"    ",
+		["=12", "|.@."],
+		["=12||31", "|"],
+		["=31", "...@....|"],
+		"   |"
+	],
+	[//row 11
+		"|   ",
+		["=22||21", "-----"],
+		["=21", "----"],
+		["=11||12||21", "-"],
+		["=11||12", "---"],
+		["=11||12||31", "-"],
+		["=31", "---------"],
+		"   |"
+	],
+	[//row 12
+		"|   ",
+		["=21", "|...@...."],
+		["=21||11", "|"],
+		["=11", ".@.|"],
+		"            |"
+	],
+	[//row 13
+		"|   ",
+		["=21", "-------"],
+		"---------          |"
+	],
+	[//row 14
+		"|          |...",
+		["=1", "@"],
+		"...|          |"
+	],
+	[//row 15
+		"|          ---------          |"
+	],
+	[//row 16
+		"-------------------------------"
+	],
+];
+
+/* TESTING THINGS OUT (in JS) */
+
+var paths = {
+	"1": {
+		label: "Castle entrance",
+		north: "11",
+	},
+	"11": {
+		label: "Castle foyer (south)",
+		north: "12",
+		west: "21",
+		south: "1",
+	},
+	"12": {
+		label: "Castle foyer (north)",
+		north: "13",
+		east: "31",
+		south: "11",
+	},
+	"13": {
+		label: "Throne room",
+		north: "14",
+		south: "12",
+	},
+	"14": {
+		label: "King Gibson's bedroom",
+		south: "13",
+	},
+	"21": {
+		label: "Workshop",
+		north: "22",
+		east: "11",
+	},
+	"22": {
+		label: "Server room",
+		north: "23",
+		south: "21",
+	},
+	"23": {
+		label: "Geothermal plant",
+		south: "22",
+	},
+	"31": {
+		label: "Great hall",
+		west: "12",
+		north: "32",
+	},
+	"32": {
+		label: "Kitchen",
+		north: "33",
+		south: "31",
+	},
+	"33": {
+		label: "Hydroponics",
+		east: "34",
+		south: "32",
+	},
+	"34": {
+		label: "Pantry",
+		west: "33",
+	},
+}
+
+var printMap = function () {
+	var string = '';
+	lispish.forEach(function (row, index) {
+		// console.log("PRINTING ROW " + index);
+		var insert = '';
+		row.forEach(function (chonk) {
+			if (Array.isArray(chonk)) {
+				if (chonk[0].includes('=')) {
+					var drawMe = false;
+					var targets = chonk[0].replace('=','').split('||');
+					targets = targets.map(function (string) {
+						return parseInt(string, 10);
+					})
+					targets.forEach(function (room) {
+							if (seenRooms[room]) {
+								drawMe = true;
+							}
+						});
+					if (drawMe) {
+						if (
+							targets[0] === location
+							&& chonk[1].includes('@')
+						) {
+							insert += chonk[1];
+						} else {
+							insert += chonk[1].replace('@', '.');
+						}
+					} else {
+						insert += ' '.repeat(chonk[1].length);
+					}
+				}
+			} else {
+				insert += chonk;
+			}
+		})
+		// console.log(insert);
+		string += insert + '\n';
+	})
+	if (paths[location]) {
+		string += `LOCATION: ${paths[location].label}\n`
+		var exits = getExitsForRoom(location);
+		string += `Exits: ${exits.join(', ')}`
+	}
+	console.log(string);
+	return string;
+};
+
+var getExitsForRoom = function (room) {
+	if (paths[room]) {
+		var exits = Object.keys(paths[room])
+			.filter(function (item) {
+				return item !== 'label'
+			})
+		return exits;
+	} else {
+		console.error('No paths found for room #' + room);
+		return false;
+	}
+};
+
+var location = 1;
+var seenRooms = {
+	"1": true,
+};
+
+var teleport = function (_room) {
+	var room = parseInt(_room, 10);
+	seenRooms[room] = true;
+	location = room;
+	printMap();
+};
+var visit = function (room) {
+	seenRooms[room] = true;
+	printMap();
+};
+var unvisit = function (room) {
+	seenRooms[room] = false;
+	printMap();
+};
+var go = function (_direction) {
+	var direction = _direction.toLocaleLowerCase();
+	var exits = getExitsForRoom(location);
+	if (exits.includes(direction)) {
+		teleport(paths[location][direction]);
+	} else if (direction === "dennis") {
+		console.log("Cheeky....");
+	} else {
+		console.log("You cannot go " + direction);
+	}
+};
+
+printMap();
+
+/* TRANSLATING MAP TO NATLANG */
+
+var makeNatlangMapRow = function (rowArray) {
+	var result = '';
+	rowArray.forEach(function (chonk, index) {
+		if (Array.isArray(chonk)) {
+			var targets = chonk[0].replace('=','').split('||');
+			var printme = chonk[1];
+			var blankprintme = ' '.repeat(printme.length);
+			var verboseTargets = targets
+				.map(function (item) {
+					return 'flag ch2-seen-room-' + item + ' is true';
+				})
+				.join(' || ')
+			result += `\tif (${verboseTargets}) {\n`;
+			if (printme.includes('@')) {
+				var splits = printme.split('@');
+				if (splits[0].length) {
+					result += `\t\tconcat serial dialog {"${splits[0]}"}\n`;
+				}
+				result += `\t\tif (variable ch2-in-room is ${targets[0]}) {\n`;
+				result += `\t\t\tconcat serial dialog {"@"}\n`;
+				result += `\t\t} else {\n`;
+				result += `\t\t\tconcat serial dialog {"."}\n`;
+				result += `\t\t}\n`;
+				if (splits[1].length) {
+					result += `\t\tconcat serial dialog {"${splits[1]}"}\n`;
+				}
+			} else {
+				result += `\t\tconcat serial dialog {"${printme}"}\n`;
+			}
+			result += `\t} else {\n`;
+			result += `\t\tconcat serial dialog {"${blankprintme}"}\n`;
+			result += `\t}\n`;
+		} else {
+			if (index === rowArray.length - 1) { // making some assumptions :P
+				result += `\tconcat serial dialog {"${chonk}\\n"}\n`;
+			} else {
+				result += `\tconcat serial dialog {"${chonk}"}\n`;
+			}
+		}
+	})
+	return result;
+}
+
+var makeNatlangMap = function () {
+	var result = 'draw-ch2-serial-map {\n';
+	result += '\tset serial control off\n';
+	lispish.forEach(function (row, index) {
+		if (index !== 0) {
+			result += '\n'
+		}
+		result += `\t// ROW ${index}\n`
+		result += makeNatlangMapRow(row);
+	})
+	result += '\tset serial control on\n'
+	result += '\tgoto null_script\n'
+	result += '}\n'
+	result = result.replace(/\\(?!n)/g, "\\\\");
+	return result;
+};
+
+console.log("breakpoint me lol")
