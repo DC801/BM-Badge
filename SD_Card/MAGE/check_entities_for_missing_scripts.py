ae7dfcfddca9427b1567354b0418150f90c24818
Alex Nibley <5225323+nibley@users.noreply.github.com>
2024-01-28 01:26:54 -0700

    Refactors, finding script definitions now working

--- SD_Card/MAGE/check_entities_for_missing_scripts.py
+++ SD_Card/MAGE/check_entities_for_missing_scripts.py
@@ -4,6 +4,20 @@ import os
 import sys
 import json
 
+'''
+TODO
+
+find out where the code for look command is. does it string match the actual entity name or is there somewhere else the mapping is specified?
+find out if it's ok for entities to have no name in map files
+find out if nativlang stipulates mgs files are ASCII encoded? most all scenario files here seems to be ASCII. maybe just from the VSCode most everyone uses
+
+programmaticaly detect entities that don't need scripts? if not just a static blacklist so they're not in the output
+generate script names and/or scripts for us when finding issues?
+use a predicate function for blacklisted_files? e.g. positive string match 'ch2'
+allow checks to be functions for more advanced checking framework? overall CI/CD conversation
+use nativlang parser JSON output to check for script definition rather than string searching mgs files (script could be referenced rather than defined, resulting in false negative for issues)
+'''
+
 # NOTE: checks to run can be added here
 checks = [
     {
@@ -17,6 +31,8 @@ checks = [
 ]
 
 def debug_to_stderr(*args, **kwargs):
+    '''print, but to stderr. DEBUG variable disables this for the whole script when False'''
+
     DEBUG = True
     # DEBUG = False
 
@@ -24,6 +40,9 @@ def debug_to_stderr(*args, **kwargs):
 
 
 def extract_entities(map_file):
+    '''return array of entities for a given map file. ignore layer groups.
+    combine entities of different objectgroup layers into one array'''
+    
     map_data = json.load(map_file)
     entities_layers = [layer for layer in map_data['layers'] if layer['type'] == 'objectgroup']
     
@@ -36,27 +55,32 @@ def extract_entities(map_file):
     return sorted(entities, key=lambda entity: entity['name']) # TODO sorted too much performance hit?
 
 
-mgs_dir = os.path.join(os.getcwd(), 'scenario_source_files/mgs')
 mgs_files = []
-for root, folder, files in os.walk(mgs_dir): # relative to this script living in SD_CARD/mage
-    for file in files:
-        if file.endswith('.mgs'):
-            mgs_files.append(os.path.join(root, file))
-
 def find_script_definition(script_name):
+    '''return bool of whether the given script name exists in an mgs file anywhere under the mgs folder'''
+
+    # setup to run once
+    if not mgs_files:
+        for root, _, files in os.walk(os.path.join(os.getcwd(), 'scenario_source_files/mgs')):
+            for file in files:
+                if file.endswith('.mgs'):
+                    mgs_files.append(os.path.join(root, file))
+
+    # find a script definition
     for mgs_file_path in mgs_files:
-        res = False
-        with open(mgs_file_path, 'r') as mgs_file:
-            return script_name in mgs_file.read()
-        
-            # for line in mgs_file:
-            #     if script_name in line:
-            #         return True
+        with open(mgs_file_path, encoding='utf-8') as mgs_file:
+            if script_name in mgs_file.read():
+                return True
+
+    return False
 
 
 def check_entity(entity):
+    '''return hierarchically indented string which is a human-readable report of
+    the issues with the given entity when running the checks specified at the top of the script'''
+
     # generate informative messages on what's missing
-    failed_checks_messages = []
+    failed_check_messages = []
 
     for check in checks:
         script_name = ''
@@ -66,32 +90,39 @@ def check_entity(entity):
             for property in entity['properties']:
                 if property['name'] == check['property_name']:
                     script_name = property['value']
-                    if (find_script_definition(script_name)):
+                    if find_script_definition(script_name):
                         found_script_definition = True
+                    break # no need to check further properties for this particular check
         
         if found_script_definition:
-            return # no output needed
+            continue # no output needed for this check. found_script_definition being true implies script_name is also set
         
-        entity_name = entity['name'] or '(NO NAME ENTITY)'
-        entity_id = entity['id']
-        fail_message = check['check_name']
+        fail_message = f'{check["check_name"]}' # the check's name serves as the beginning of the fail message
         if script_name: # a script name is present in map data but never defined in mgs folder
-            fail_message += f' (map file promised \'{script_name}\') '
-        else: # script name is not even present
-            fail_message += ' (map file has no binding)'
+            global num_undefined_scripts
+            num_undefined_scripts += 1
+            fail_message += f' (map file gives \'{script_name}\' but no definition)'
 
-        failed_checks_messages.append(fail_message)
+        failed_check_messages.append(fail_message)
 
-    # print out the messages
-    if not failed_checks_messages:
-        return # no output needed
+    # combine the messages into one string
+    if not failed_check_messages:
+        return '' # no output needed - entity passed every check. don't add to the message to be printed or increment counts
     
-    global num_entities
-    num_entities += 1
+    global num_problem_entities
+    num_problem_entities += 1
+
+    entity_name = entity['name']
+    if not entity_name:
+        entity_name = 'NO-NAME ENTITY'
+        global num_no_name_entities
+        num_no_name_entities += 1
     
-    print(f'    {entity_name} (#{entity_id}) needs:')
-    for fail_message in failed_checks_messages:
-        print(f'        {fail_message}')
+    result = f'    {entity_name} (#{entity["id"]}) needs:\n'
+    for failed_check_message in failed_check_messages:
+        result += f'        {failed_check_message}\n'
+
+    return result
 
 
 
@@ -100,29 +131,38 @@ def check_entity(entity):
 ### main work
 
 # get all map files
-map_dir = os.path.join(os.getcwd(), 'scenario_source_files/maps') # relative to this script living in SD_CARD/mage
-
 blacklisted_files = ['map-16px_dungeon.json', 'map-action_testing_01.json', 'map-action_testing_02.json', 'map-bakery.json', 'map-bling-dc801.json', 'map-bling-digi-mage.json', 'map-bling-qr.json', 'map-bling-zero.json', 'map-bobsclub.json', 'map-credits.json', 'map-credits2.json', 'map-demo.json', 'map-dialog_codec.json', 'map-dialog_moon.json', 'map-family.json', 'map-flying-toasters.json', 'map-greenhouse.json', 'map-lodge.json', 'map-magehouse-birthday.json', 'map-magehouse.json', 'map-main.json', 'map-main2.json', 'map-main_menu.json', 'map-mini_dungeon.json', 'map-oldcouplehouse.json', 'map-secretroom.json', 'map-test.json', 'map-testbig.json', 'map-town.json', 'map-warp_zone.json', 'map-woprhouse.json']
-# TODO use a predicate function instead? e.g. positive string match 'ch2'
 
+map_dir = os.path.join(os.getcwd(), 'scenario_source_files/maps')
 map_dir_contents = [os.path.join(map_dir, f) for f in sorted(os.listdir(map_dir)) if f not in blacklisted_files]
-map_file_paths = [f for f in map_dir_contents if os.path.isfile(os.path.join(map_dir, f))]
+map_file_paths = [f for f in map_dir_contents if os.path.isfile(f)]
 
 # extract entities out of each file
-num_entities = 0
+num_problem_maps = 0
+num_problem_entities = 0
+num_no_name_entities = 0
+num_undefined_scripts = 0
 
 for map_file_path in map_file_paths:
     map_file_basename = os.path.basename(map_file_path)
 
-    with open(map_file_path, 'r') as map_file:
+    with open(map_file_path, encoding='utf-8') as map_file:
         map_file_entities = extract_entities(map_file)
 
-    print(f'{len(map_file_entities):<3} entities in {map_file_basename}')
+    map_problem_message = ''
 
+    # report issues found with each entity
     for entity in map_file_entities:
-        check_entity(entity)
-    
-    print()
+        map_problem_message += check_entity(entity)
 
-print('')
-print(f'Found issues with {num_entities} total entities')
+    if not map_problem_message:
+        continue # no output needed for the entire map, don't print map filename or increment counts
+
+    num_problem_maps += 1
+    print(map_file_basename)
+    print(map_problem_message)
+    
+
+print(f'Found issues with {num_problem_entities} total entities in {num_problem_maps} total maps')
+print(f'Found {num_undefined_scripts} total scripts that never got defined')
+print(f'Found {num_no_name_entities} total entities with no name')
