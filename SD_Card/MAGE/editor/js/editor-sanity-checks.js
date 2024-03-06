97e0bafc1241b83d732453059f07515e32c1992e
Alex Nibley <5225323+nibley@users.noreply.github.com>
2024-03-05 21:26:47 -0700

    first decently working GUI. need to check for script definitions

--- SD_Card/MAGE/editor/js/editor-sanity-checks.js
+++ SD_Card/MAGE/editor/js/editor-sanity-checks.js
@@ -1,12 +1,23 @@
 /*
 TODO
 
+definitions checking
+---
+implement for JS
+check for script definitions in JSON files as well as MGS files (there are a couple)
+
 misc
 ---
+ask about populating sanityChecks onto scenarioData
+presentation design
+- original design: a report like the python output that would be useful to download
+	- fixes separate from problems
+- new design: Vue component for each problem
+	- fixes presented next to problems
 problem counts for GUI and CLI
 - final print-out info in GUI?
 CLI print-out
-txt download next to copy button
+txt download next to copy button? (ask)
 separate js files for components instead of one?
 how will look work for multiple entities of the same or similar names (eg bread, torch)
 gameengine prettier mage_command_control.cpp:185
@@ -24,13 +35,9 @@ generating script names
 - nice indenting to be able to paste into maps
 - what to do with a no-name entity?
 - check if name we generate was already used (like above, probably good enough to output to a separate section needing manual fixing)
-use fallback entity names that are generated somewhere late in the build process for fixes? (saw them as something like entity-5)
-
-definitions checking
----
-check for script definitions in JSON files as well as MGS files (there are a couple)
+use fallback entity names that are generated somewhere late in the build process for fixes? (e.g. 'Mage 32')
+- are these only showing up sometimes? 
 */
-
 
 
 
@@ -54,7 +61,6 @@ Vue.component('editor-sanity-check-problem', {
 	methods: {
 		collapse: function () {
 			this.collapsed = !this.collapsed;
-			console.log('XXX', 'collapse one problem');
 		},
 		copyFixes: function () {
 			this.$refs.copyFixesText.select();
@@ -64,36 +70,10 @@ Vue.component('editor-sanity-check-problem', {
 			// TODO
 		}
 	},
-/*
-TODO copy button
-
-<div class="
-	alert
-	alert-dismissible
-	show
-"
-role="alert"
->
-	<span>You can click the "Copy" button to the right to copy these suggested fixes.</span>
-	<button
-		type="button"
-		class="close"
-		title="Copy"
-		@click="copyFixes"
-	>
-		<span aria-hidden="true">📋</span>
-	</button>
-</div>
-<textarea
-	ref="copyFixesText"
-></textarea>
-*/
 	template: /*html*/`
-<div
-	class="card"
->
+<div class="card mb-1 text-white">
 	<div class="card-header bg-primary">
-		hi TODO
+		{{problem.name || "NO NAME"}} (id {{problem.id}})
 		<span
 			class="position-absolute"
 			style="top:6px; right:6px;"
@@ -105,11 +85,89 @@ role="alert"
 			>_</button>
 		</span>
 	</div>
-	<div class="card-body p-3">
-		{{problem.name || "NO NAME"}} (id {{problem.id}}) needs {{problem.problemMessage}}
+	<div
+		class="card-body p-1"
+		v-if="!collapsed"
+	>
+		<p>{{problem.name || "NO NAME"}} (id {{problem.id}}) needs {{problem.problemMessage}}</p>
+		<p>You can click the button to the right to copy these suggested fixes.</p>
+		<textarea ref="copyFixesText">
+			TODO generated fixes
+		</textarea>
+		<button
+			type="button"
+			class="close"
+			title="Copy"
+			@click="copyFixes"
+		>
+			<span aria-hidden="true">📋</span>
+		</button>
 	</div>
 </div>
 `});
+
+
+
+
+
+
+
+
+
+
+
+
+Vue.component('editor-sanity-check-map', {
+	name: 'editor-sanity-check-map',
+	props: {
+		mapName: {
+			type: String,
+			required: true
+		},
+		problems: {
+			type: Array,
+			required: true
+		}
+	},
+	data: function () {
+		return {
+			collapsed: true
+		}
+	},
+	methods: {
+		collapse: function () {
+			this.collapsed = !this.collapsed;
+		}
+	},
+	template: /*html*/`
+<div class="card mb-1 text-white">
+	<div class="card-header bg-primary">
+		Problems in map '{{mapName}}' ({{problems.length}} entities)
+		<span
+			class="position-absolute"
+			style="top:6px; right:6px;"
+		>
+			<button
+				type="button"
+				class="btn btn-outline-info"
+				@click="collapse"
+			>_</button>
+		</span>
+	</div>
+	<div
+		class="card-body"
+		v-if="!collapsed"
+	>
+		<editor-sanity-check-problem
+			v-for="problem in problems"
+			:key="problem.id"
+			:problem="problem"
+		></editor-sanity-check-problem>
+	</div>
+</div>
+`});
+
+
 
 
 
@@ -136,15 +194,12 @@ Vue.component('editor-sanity-check', {
 	methods: {
 		collapse: function () {
 			this.collapsed = !this.collapsed;
-			console.log('XXX', 'collapse one check');
 		}
 	},
 	template: /*html*/`
-<div
-	class="card"
->
+<div class="card mb-1 text-white">
 	<div class="card-header bg-primary">
-		Problems with {{checkName}}
+		Problems with '{{checkName}}' ({{Object.keys(maps).length}} maps)
 		<span
 			class="position-absolute"
 			style="top:6px; right:6px;"
@@ -156,28 +211,20 @@ Vue.component('editor-sanity-check', {
 			>_</button>
 		</span>
 	</div>
-	<div class="card-body p-3">
-		<div
-			class="card"
-			v-for="(mapProblems, mapName) in maps"
-			:key="mapName"		
-		>
-			<div class="card-header bg-primary">
-				Problems in map {{mapName}}
-			</div>
-			<div class="card-body">
-				<ul>
-					<editor-sanity-check-problem
-						v-for="problem in mapProblems"
-						:key="problem.id"
-						:problem="problem"
-					></editor-sanity-check-problem>
-				</ul>
-			</div>
-		</div>
+	<div
+		class="card-body p-1"
+		v-if="!collapsed"
+	>
+		<editor-sanity-check-map
+			v-for="(problems, mapName) in maps"
+			:key="mapName"
+			:map-name="mapName"
+			:problems="problems"
+		></editor-sanity-check-map>
 	</div>
 </div>
 `});
+
 
 
 
@@ -195,19 +242,16 @@ Vue.component('editor-sanity-checks', {
 	},
 	data: function () {
 		return {
-			collapsed: false, // TODO change
+			collapsed: true
 		}
 	},
 	methods: {
 		collapse: function () {
 			this.collapsed = !this.collapsed;
-			console.log('XXX', 'collapse the checks overall');
 		}
 	},
 	template: /*html*/`
-<div
-	class="card text-white mb-3"
->
+<div class="card mb-1 text-white">
 	<div class="card-header">
 		Additional reports about the build
 		<span
@@ -226,9 +270,10 @@ Vue.component('editor-sanity-checks', {
 		v-if="!collapsed"
 	>
 		<editor-sanity-check
-			v-for="(checkProblems, checkName) in problems"
+			v-for="(maps, checkName) in problems"
 			:key="checkName"
 			:check-name="checkName"
-			:maps="checkProblems"></editor-sanity-check>
+			:maps="maps"
+		></editor-sanity-check>
 	</div>
 </div>`});
