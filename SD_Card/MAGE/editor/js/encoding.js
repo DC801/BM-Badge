6a57f65f01b32faeb0497d0ce624c8a31f59a7f7
Alex Nibley <5225323+nibley@users.noreply.github.com>
2024-03-13 19:16:32 -0600

    stash

--- SD_Card/MAGE/editor/js/encoding.js
+++ SD_Card/MAGE/editor/js/encoding.js
@@ -16,6 +16,39 @@ var dataTypes = [
 	'images',
 ];
 
+var checkMapEntityPropertyPresent = function(compositeEntity, propertyToCheck) {
+	var scriptName = '';
+	var foundScriptDefinition = false; // TODO
+
+	if (! ('properties' in compositeEntity)) {
+		return null;
+	}
+
+	for (var index in compositeEntity.properties) {
+		var entityProperty = compositeEntity.properties[index];
+		if (entityProperty.name == propertyToCheck) {
+			scriptName = entityProperty.value;
+			// if (findScriptDefinition(scriptName)) {
+			// 	foundScriptDefinition = true;
+			// } // TODO
+			break; // no need to search for the right property object more after finding it
+		}
+	}
+
+	if (foundScriptDefinition) {
+		return null; // no problem found for this check
+	} // note foundScriptDefinition being true implies scriptName is also set
+
+	var result = `${compositeEntity.name || "NO NAME"} (id ${compositeEntity.id}) needs a ${propertyToCheck} script`;
+
+	if (scriptName) { // a script name is present in map data but never defined in mgs folder
+		// num_undefined_scripts += 1; // TODO
+		result += ` (UNDEFINED: script \'${scriptName}\' expected from the map file is never defined)`;
+	}
+
+	return result;
+};
+
 var handleScenarioData = function (fileNameMap) {
 	return function (scenarioData) {
 		console.log(
@@ -40,82 +73,18 @@ var handleScenarioData = function (fileNameMap) {
 			problems: {},
 			checks: {
 				checkInteractScript: function(compositeEntity) {
-					var propertyToCheck = 'on_interact';
-		
-					var scriptName = '';
-					var foundScriptDefinition = false; // TODO
-		
-					if (! ('properties' in compositeEntity)) {
-						return null;
-					}
-		
-					for (propertyName in compositeEntity.properties) {
-						if (Object.hasOwnProperty.call(compositeEntity.properties, propertyName)) {
-							if (propertyName == propertyToCheck) {
-								scriptName = compositeEntity.properties[propertyName];
-								// if (findScriptDefinition(scriptName)) {
-								// 	foundScriptDefinition = true;
-								// } // TODO
-								break; // no need to search for the right property object more after finding it
-							}
-						}
-					}
-		
-					if (foundScriptDefinition) {
-						return null; // no problem found for this check
-					} // note foundScriptDefinition being true implies scriptName is also set
-		
-					var result = `${propertyToCheck} script`;
-		
-					if (scriptName) { // a script name is present in map data but never defined in mgs folder
-						// num_undefined_scripts += 1; // TODO
-						result += ` (UNDEFINED: script \'${scriptName}\' expected from the map file is never defined)`;
-					}
-		
-					return result;
+					return checkMapEntityPropertyPresent(compositeEntity, 'on_interact');
 				},
 				checkLookScript: function(compositeEntity) {
-					var propertyToCheck = 'on_look';
-		
-					var scriptName = '';
-					var foundScriptDefinition = false; // TODO
-		
-					if (! ('properties' in compositeEntity)) {
-						return null;
-					}
-		
-					for (propertyName in compositeEntity.properties) {
-						if (Object.hasOwnProperty.call(compositeEntity.properties, propertyName)) {
-							if (propertyName == propertyToCheck) {
-								scriptName = compositeEntity.properties[propertyName];
-								// if (findScriptDefinition(scriptName)) {
-								// 	foundScriptDefinition = true;
-								// } // TODO
-								break; // no need to search for the right property object more after finding it
-							}
-						}
-					}
-		
-					if (foundScriptDefinition) {
-						return null; // no problem found for this check
-					} // note foundScriptDefinition being true implies scriptName is also set
-		
-					var result = `${propertyToCheck} script`;
-		
-					if (scriptName) { // a script name is present in map data but never defined in mgs folder
-						// num_undefined_scripts += 1; // TODO
-						result += ` (UNDEFINED: script \'${scriptName}\' expected from the map file is never defined)`;
-					}
-		
-					return result;
+					return checkMapEntityPropertyPresent(compositeEntity, 'on_look');
 				},
 				checkNamePresent: function(compositeEntity) {
 					if (compositeEntity.name == undefined || compositeEntity.name == null || ! (compositeEntity.name.replace(/\s/g, '').length)) {
-						return 'a name in the map file';
+						return `${compositeEntity.name || "NO NAME"} (id ${compositeEntity.id}) needs a name in the map file`;
 					} else {
 						return null;
 					}
-				},
+				}
 			}
 		};
 		var preloadSkinsPromise = preloadAllDialogSkins(fileNameMap, scenarioData);
