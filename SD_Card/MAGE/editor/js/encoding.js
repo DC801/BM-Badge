21ae8e04d6c93b80eb84a080d860b57b764462e4
Alex Nibley <5225323+nibley@users.noreply.github.com>
2024-03-16 23:23:53 -0600

    refactor: less in encoding.js, new file warnings.js, and change nomenclature from sanity checks to warnings

--- SD_Card/MAGE/editor/js/encoding.js
+++ SD_Card/MAGE/editor/js/encoding.js
@@ -16,39 +16,6 @@ var dataTypes = [
 	'images',
 ];
 
-var checkMapEntityPropertyPresent = function(compositeEntity, propertyToCheck) {
-	var scriptName = '';
-	var foundScriptDefinition = false; // TODO
-
-	if (! ('properties' in compositeEntity)) {
-		return null;
-	}
-
-	for (var index in compositeEntity.properties) {
-		var entityProperty = compositeEntity.properties[index];
-		if (entityProperty.name == propertyToCheck) {
-			scriptName = entityProperty.value;
-			// if (findScriptDefinition(scriptName)) {
-			// 	foundScriptDefinition = true;
-			// } // TODO
-			break; // no need to search for the right property object more after finding it
-		}
-	}
-
-	if (foundScriptDefinition) {
-		return null; // no problem found for this check
-	} // note foundScriptDefinition being true implies scriptName is also set
-
-	var result = `${compositeEntity.name || "NO NAME"} (id ${compositeEntity.id}) needs a ${propertyToCheck} script`;
-
-	if (scriptName) { // a script name is present in map data but never defined in mgs folder
-		// num_undefined_scripts += 1; // TODO
-		result += ` (UNDEFINED: script \'${scriptName}\' expected from the map file is never defined)`;
-	}
-
-	return result;
-};
-
 var handleScenarioData = function (fileNameMap) {
 	return function (scenarioData) {
 		console.log(
@@ -69,24 +36,7 @@ var handleScenarioData = function (fileNameMap) {
 			scenarioData.parsed[typeName] = [];
 		});
 		scenarioData.dialogSkinsTilesetMap = {}
-		scenarioData.sanityChecks = {
-			problems: {},
-			checks: {
-				checkInteractScript: function(compositeEntity) {
-					return checkMapEntityPropertyPresent(compositeEntity, 'on_interact');
-				},
-				checkLookScript: function(compositeEntity) {
-					return checkMapEntityPropertyPresent(compositeEntity, 'on_look');
-				},
-				checkNamePresent: function(compositeEntity) {
-					if (compositeEntity.name == undefined || compositeEntity.name == null || ! (compositeEntity.name.replace(/\s/g, '').length)) {
-						return `${compositeEntity.name || "NO NAME"} (id ${compositeEntity.id}) needs a name in the map file`;
-					} else {
-						return null;
-					}
-				}
-			}
-		};
+		scenarioData.warnings = {};
 		var preloadSkinsPromise = preloadAllDialogSkins(fileNameMap, scenarioData);
 		var portraitsFile = fileNameMap['portraits.json'];
 		var portraitsPromise = preloadSkinsPromise.then(function preloadSkinHandler () {
