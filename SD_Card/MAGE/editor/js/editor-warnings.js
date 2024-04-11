fe2f6fa07fc88962e0b5b0ab388ec4ca5230bf0e
Alex Nibley <5225323+nibley@users.noreply.github.com>
2024-04-11 14:35:03 -0600

    create component for the innermost layer (single entity)

--- SD_Card/MAGE/editor/js/editor-warnings.js
+++ SD_Card/MAGE/editor/js/editor-warnings.js
@@ -31,6 +31,40 @@ move look scripts not in the look scripts file? e.g. see ch2-castle-34.mgs
 - (also naming of script look-ch2-castle-34 but it's really for the pantry?)
 */
 
+Vue.component('editor-warning', {
+	name: 'editor-warning',
+	props: {
+		entity: {
+			required: true,
+			type: Object,
+		},
+	},
+	template: /*html*/`
+<div
+	class="editor-warnings-warning card text-white border-secondary my-3"
+>
+	<div class="card-header bg-secondary">Entity &grave;{{entity.name || 'NO NAME'}}&grave; (id {{entity.id}})</div>
+	<div class="card-body px-3 pt-3 pb-2">
+		<p>{{ entity.warningMessage }}</p>
+		<label v-if="entity.fixes.length">Click the button by any of these fixes to copy it.</label>
+		<div
+			v-for="(fix, fixIndex) in entity.fixes"
+			:key="fixIndex"
+		>
+			<div class="row align-items-center flex-nowrap px-2">
+				<pre class="border border-primary rounded p-2 w-100">{{fix.fixText}}</pre>
+				<copy-button
+					:text="fix.fixText"
+					class="ml-1"
+					style="width: 2rem;"
+				></copy-button>
+			</div>
+		</div>
+	</div>
+</div>
+`});
+
+
 Vue.component('editor-warnings', {
 	name: 'editor-warnings',
 	props: {
@@ -42,7 +76,7 @@ Vue.component('editor-warnings', {
 	computed: {
 		warningsSorted: function() {
 			// convert warnings data structure to its 2D array equivalent
-			// (sorted lexically by check or map name, or by `id` for entities)
+			// (sorted at each layer, lexically for check names and map names, and by `id` for entities)
 			// precomputing this is useful for ordered access and quicker length checks
 			var sortByNameInIndexZero = function(a, b) {
 				return a[0].localeCompare(b);
@@ -64,7 +98,8 @@ Vue.component('editor-warnings', {
 		},
 	},
 	template: /*html*/`
-<div class="editor-warnings card text-white mb-3">
+<div
+	class="editor-warnings card text-white mb-3">
 	<div class="card-header bg-primary">Additional reports about the build ({{warningsSorted.length}} checks)</div>
 	<div class="card-body py-1">
 		<!-- "invisible wrapper" use of <template> because of v-for inside (good practice) -->
@@ -79,30 +114,11 @@ Vue.component('editor-warnings', {
 					:key="mapName"
 					:title="'Problems in map &grave;' + mapName + '&grave; (' + entities.length  + ' entities)'"
 				>
-					<div
-						class="warnings-warning card text-white border-secondary my-3"
+					<editor-warning
+						:entity="entity"
 						v-for="entity in entities"
 						:key="entity.id"
-					>
-						<div class="card-header bg-secondary">Entity &grave;{{entity.name || 'NO NAME'}}&grave; (id {{entity.id}})</div>
-						<div class="card-body px-3 pt-3 pb-2">
-							<p>{{ entity.warningMessage }}</p>
-							<label v-if="entity.fixes.length">Click the button by any of these fixes to copy it.</label>
-							<div
-								v-for="(fix, fixIndex) in entity.fixes"
-								:key="fixIndex"
-							>
-								<div class="row align-items-center flex-nowrap px-2">
-									<pre class="border border-primary rounded p-2 w-100">{{fix.fixText}}</pre>
-									<copy-button
-										:text="fix.fixText"
-										class="ml-1"
-										style="width: 2rem;"
-									></copy-button>
-								</div>
-							</div>
-						</div>
-					</div>
+					></editor-warning>
 				</editor-accordion>
 			</editor-accordion>
 		</template >
